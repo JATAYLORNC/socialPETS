2b729b10123c9d3bc39e61efa4dd60ee12240946
JATAYLORNC <disneyjeff@gmail.com>
2018-06-14 09:29:49 -0400

    corrected /api/index.js to remove mount path from router.use statement

--- routes/API/auth.js
+++ routes/API/auth.js
@@ -1,5 +1,4 @@
 var router = require("express").Router();
-var passport = require("../../passport");
 var authController = require("../../controllers/authController");
 
   // // this route is just used to get the user basic info
