a1a359b0dccd82149402097482720d56c807b430
Stephanie <stephanie.lawrie@dpi.nc.gov>
2018-06-14 20:03:11 -0400

    First Initial

--- db/models/Pet.js
+++ db/models/Pet.js
@@ -7,8 +7,29 @@ var Schema = mongoose.Schema;
 // This is similar to a Sequelize model
 var PetSchema = new Schema({
   // `body` is of type String
-  body: String
-});
+  name: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  breed: String,
+  age: Number,
+  gender: String,
+  size: String,
+  rescuePet: Boolean,
+  favoriteActivities: [String],
+  favoriteToys: [String],
+  specialTalent: String,
+  Posts: [{
+    type: Schema.Types.ObjectId,
+    ref: "Posts"
+  }],
+  friendsId: [Number],
+},
+  {timestamps: true});
 
 // This creates our model from the above schema, using mongoose's model method
 var Pet = mongoose.model("Pet", PetSchema);
