c65bcbb192f406e4dcbfc253859183e3ff001f00
gsmaughan <gmwccgolf@gmail.com>
2018-06-16 10:40:39 -0400

    post updated to find file

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -4,6 +4,13 @@ import '../styles/post.css';
 
 class Post extends Component {
 
+handleSubmit = () => {
+    console.log("handleSubmit fired");
+{/*upload videos or files*/}
+{/*axios POST route*/}
+
+}
+
 render() {
 
     return (
@@ -14,12 +21,25 @@ render() {
                         <form className="form">
                             <div className="form-group">
                                 <label for="makePost">Make a Post</label>
+                                   
+                                    <ul className="nav">
+                                        <li class="nav-item" idName="upload" >
+                                           
+                                            <div className="file">Upload Image or Video
+                                                <input type="file" id="getFile" />
+                                            </div>
+
+                                        </li>
+                
+                                    </ul>
+
                                     <div>
-                                    <textarea className="text"  name="Text1" cols="40" rows="5" placeholder="What's on your mind?"></textarea>
+                                        <textarea className="text"  name="Text1" cols="40" rows="5" placeholder="What's on your mind?"></textarea>
                                     </div>
+                                    <
                             </div>
  
-                            <button type="submit" class="btn btn-primary">Submit</button>
+                            <button type="submit" className="btn btn-primary" onClick={this.handleSubmit} >Submit</button>
                         </form>
     				</div>
     			</div>
