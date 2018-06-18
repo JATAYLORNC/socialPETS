ea6a8084e0ca59a9b81c14171d17668b218dc450
JATAYLORNC <disneyjeff@gmail.com>
2018-06-18 18:18:15 -0400

    update to passport/index.js

--- passport/index.js
+++ passport/index.js
@@ -4,32 +4,28 @@ import User from "../db/models/User";
 
 passport.serializeUser((user, done) => {
 	console.log(`=== serialize ... called ===`);
-	console.log(user); // the whole raw user object!
 	console.log(`---------`);
 	done(null, { _id: user._id });
 });
 
-// passport.deserializeUser((id, done) => {
-// 	console.log(`DEserialize ... called`)
-// 	User.findOne(
-// 		{ _id: id },
-// 		'email',
-// 		(err, user) => {
-// 			console.log(`======= DESERIALIZE USER CALLED ======`);
-// 			console.log(user);
-// 			console.log(`--------------`);
-// 			done(null, user);
-// 		}
-// 	);
-// });
-
 passport.deserializeUser((id, done) => {
-	User.findById(id).populate('Pet').exec((err, user) => {
-		console.log(`======= DESERIALIZE USER CALLED ======`);
-		console.log(user.Pet);
-		done(err, user);
-	});
+	console.log(`DEserialize ... called`)
+	User.findOne(
+		{ _id: id },
+		'email',
+		(err, user) => {
+			console.log(`======= DESERIALIZE USER CALLED ======`);
+			done(null, user);
+		}
+	);
 });
+
+// passport.deserializeUser((id, done) => {
+// 	User.findById(id).exec((err, user) => {
+// 		console.log(`======= DESERIALIZE USER CALLED ======`);
+// 		done(err, user);
+// 	});
+// });
 
 // ==== Register Strategies ====
 passport.use(LocalStrategy);
