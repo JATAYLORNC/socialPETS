a2596c3bc1925f33a6543d1db4e2d4bbdfce8e14
JATAYLORNC <disneyjeff@gmail.com>
2018-06-18 15:23:24 -0400

    enabled pet information to populate navbar menu on login

--- passport/index.js
+++ passport/index.js
@@ -9,18 +9,26 @@ passport.serializeUser((user, done) => {
 	done(null, { _id: user._id });
 });
 
+// passport.deserializeUser((id, done) => {
+// 	console.log(`DEserialize ... called`)
+// 	User.findOne(
+// 		{ _id: id },
+// 		'email',
+// 		(err, user) => {
+// 			console.log(`======= DESERIALIZE USER CALLED ======`);
+// 			console.log(user);
+// 			console.log(`--------------`);
+// 			done(null, user);
+// 		}
+// 	);
+// });
+
 passport.deserializeUser((id, done) => {
-	console.log(`DEserialize ... called`)
-	User.findOne(
-		{ _id: id },
-		'email',
-		(err, user) => {
-			console.log(`======= DESERIALIZE USER CALLED ======`);
-			console.log(user);
-			console.log(`--------------`);
-			done(null, user);
-		}
-	);
+	User.findById(id).populate('Pet').exec((err, user) => {
+		console.log(`======= DESERIALIZE USER CALLED ======`);
+		console.log(user.Pet);
+		done(err, user);
+	});
 });
 
 // ==== Register Strategies ====
