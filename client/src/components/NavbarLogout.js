198c0c702258fb7a6c9e691da7e9594fc772bdfa
Colin Williams <colin_m_williams@hotmail.com>
2018-06-18 21:42:46 -0400

    Added PetProfile component

--- client/src/components/NavbarLogout.js
+++ client/src/components/NavbarLogout.js
@@ -2,41 +2,71 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/navbar.css";
 
-const NavbarLogout = (props) => (
+const NavbarLogout = props => (
   <nav className="navbar navbar-expand-md w-100" id="myNavbar">
     <div className="container-fluid row d-flex align-items-center">
       <div className="col-sm-3">
-        <Link className="navbar-brand text-dark" id="brand" to="/"><strong>socialPETS</strong></Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
+        <Link className="navbar-brand text-dark" id="brand" to="/">
+          <strong>socialPETS</strong>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
         </button>
       </div>
       <div className="col-sm-4">
         <div className="input-group my-2 my-lg-0">
-            <input className="form-control-sm" id="search" type="search" placeholder="Search" aria-label="Search" />
-            <div className="input-group-append">
-              <span className="input-group-text btn bg-dark text-white" type="button"><i className="fas fa-search"></i></span>
-            </div>
+          <input className="form-control-sm" id="search" type="search" placeholder="Search" aria-label="Search" />
+          <div className="input-group-append">
+            <span className="input-group-text btn bg-dark text-white" type="button">
+              <i className="fas fa-search" />
+            </span>
+          </div>
         </div>
       </div>
       <div className="col-sm-4 collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav d-flex justify-content-start align-items-center">
           <li className="nav-item mr-2">
-            <Link className="nav-link text-dark menuText" to="/home" id="home">Home</Link>
+            <Link className="nav-link text-dark menuText" to="/home" id="home">
+              Home
+            </Link>
           </li>
           <li className="nav-item dropdown mr-2">
-            <Link className="nav-link dropdown-toggle text-dark menuText" role="button" data-toggle="dropdown"  to="/Profile" id="saved">Profile</Link>
+            <Link
+              className="nav-link dropdown-toggle text-dark menuText"
+              role="button"
+              data-toggle="dropdown"
+              to="/Profile"
+              id="saved"
+            >
+              Profile
+            </Link>
             <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-            {props.user.Pet.map((pet) => <Link key={pet._id.toString()} className="dropdown-item" to={`/profile/${pet._id}`} >{pet.name}</Link>)}
+              {props.user.Pet.map(pet => (
+                <Link key={pet._id.toString()} className="dropdown-item" to={`/profile/${pet._id}`}>
+                  {pet.name}
+                </Link>
+              ))}
             </div>
           </li>
           <li>
-            <Link className="nav-link text-dark menuText" to="/addpet" id="addpet">Add A Pet</Link>
+            <Link className="nav-link text-dark menuText" to="/addpet" id="addpet">
+              Add A Pet
+            </Link>
           </li>
         </ul>
       </div>
       <div className="col-sm-1">
-        <button className="btn btn-sm btn-dark ml-2 menuButton" onClick={props._logout}>Log Out</button>
+        <button className="btn btn-sm btn-dark ml-2 menuButton" onClick={props._logout}>
+          Log Out
+        </button>
       </div>
     </div>
   </nav>
---
+++ client/src/components/PetProfile.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "../styles/petprofile.css";
+import Card from "./Card";
+import Post from "./Post";
+
+class PetProfile extends React.Component {
+  addProfileImages = () => {
+    // needs code to add the main profile image and the background profile image.
+  };
+
+  getProfileImages = () => {
+    // needs logic to retrive the images from the database.
+  };
+
+  render() {
+    return (
+      <div className="PetProfile">
+        <div className="row">
+          {/* <!-- left column --> */}
+          <div className="col-sm-3" />
+
+          {/* <!-- Main Content column --> */}
+          <div className="col-sm-6">
+            {/* <!-- jumbotron for profile header --> */}
+            <div className="row">
+              <div className="col-sm-12">
+                <div className="jumbotron">
+                  <h1 className="display-4">{this.props.name}</h1>
+                  <img className="img-thumbnail" src="http://via.placeholder.com/150x150" alt="PetProfile Name" />
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* <!-- POST component --> */}
+          <div className="row">
+            <div className="col-sm-12">
+              <Post />
+            </div>
+          </div>
+
+          {/* <!-- social activity CARDS --> */}
+          <div className="row">
+            <div className="col-sm-12">{/* <Card /> */}</div>
+          </div>
+
+          {/* <!-- right column --> */}
+          <div className="col-md-3" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PetProfile;
