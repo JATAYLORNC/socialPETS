51c1a601dcdf7e8ffb9d766e6293ae8fcf78511d
JATAYLORNC <disneyjeff@gmail.com>
2018-06-28 10:32:05 -0400

    enabled 'Following' on homeRight to list pets by owner

--- client/src/components/HomeRight.js
+++ client/src/components/HomeRight.js
@@ -2,28 +2,74 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/homeRight.css";
 
-const HomeRight = (props) => (
+class HomeRight extends React.Component {
 
-  <div className="row">
-    <div className="col-sm-12" style={{width: '250px', marginLeft: '80px', position: 'fixed', top: '17%', bottom: '0', borderLeft: '1px solid lightgray'}}>
-      <div className="pl-3">
-        <h5 style={{marginTop: '25%', paddingTop: '70px'}}>Following:</h5>       
-            
-        {props.friends.map((friend) => {
-          return (
-            <div>
-              <p><strong>{`${friend.User[0].firstname} ${friend.User[0].lastname}`}</strong></p>
-              <Link className="text-dark" key={friend._id} to={`/profile/${friend._id}`} >
-                <img src="http://via.placeholder.com/40x40" className="img-responsive rounded-circle petImage mr-3" alt="Pet" />
-                <span>{friend.name}</span>
-                <br /><br />
-              </Link>
-            </div>
-          )
-        })}
-      </div> 
-    </div>
-  </div>
-);
+  state = {
+    petFriends: []
+  }
+
+  componentDidMount = () => {
+		this.invertArray();
+	}	
+
+
+  invertArray = () => {
+    let users=[];
+    let petFriends=[]
+    this.props.friends.forEach(friend => {
+      if(!users.includes(friend.User[0]._id)) {
+        users.push(friend.User[0]._id);
+        let myfriend={
+          fullName: `${friend.User[0].firstname} ${friend.User[0].lastname}`,
+          pet: [friend]
+        }
+        petFriends.push(myfriend);
+      } else {
+        let fullname=`${friend.User[0].firstname} ${friend.User[0].lastname}`
+        petFriends = petFriends.map((userpet) => {
+          if(userpet.fullname) {
+            userpet.pet.push(friend);
+            return userpet;
+          } else {
+            return userpet;
+          }
+        });
+      }
+    });
+    this.setState({
+      petFriends: petFriends
+    });
+  }
+
+  render () {
+    return (
+
+      <div className="row">
+        <div className="col-sm-12" style={{width: '250px', marginLeft: '80px', position: 'fixed', top: '17%', bottom: '0', borderLeft: '1px solid lightgray'}}>
+          <div className="pl-3">
+            <h5 style={{marginTop: '25%', paddingTop: '70px'}}>Following:</h5>       
+            {this.state.petFriends.map((friend) => {
+              return (
+                <div>
+                  <p><strong>{`${friend.fullName}`}</strong></p>
+                  {friend.pet.map(pet => {
+                    return (
+                    <Link className="text-dark" key={pet._id} to={`/profile/${pet._id}`} >
+                      <img src="http://via.placeholder.com/40x40" className="img-responsive rounded-circle petImage mr-3" alt="Pet" />
+                      <span>{pet.name}</span>
+                      <br /><br />
+                    </Link>
+                    );
+                  })}
+                </div>
+              )
+            })}
+          </div> 
+        </div>
+      </div>
+    );
+
+  }
+}
 
 export default HomeRight;
