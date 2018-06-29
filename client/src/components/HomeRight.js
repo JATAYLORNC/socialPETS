f7bfb46dd788000329e96a06d51b584a66ea05c9
Michael Dinh <mkdinh94@gmail.com>
2018-06-29 08:51:12 -0400

    fix server's routing

--- client/src/components/HomeRight.js
+++ client/src/components/HomeRight.js
@@ -3,34 +3,34 @@ import { Link } from "react-router-dom";
 import "../styles/homeRight.css";
 
 class HomeRight extends React.Component {
-
   state = {
-    petFriends: []
-  }
+    petFriends: [],
+  };
 
   componentDidMount = () => {
-		this.invertArray();
-	}	
-
+    this.invertArray();
+  };
 
   invertArray = () => {
-    let users=[];
-    let petFriends=[]
-    if(this.props.Friends[0])
-      if(this.props.friends[0].User[0]) {
+    let users = [];
+    let petFriends = [];
+    if (this.props.Friends[0])
+      if (this.props.friends[0].User[0]) {
         this.props.friends.forEach(friend => {
-          console.log("friend", friend)
-          if(!users.includes(friend.User[0]._id)) {
+          console.log("friend", friend);
+          if (!users.includes(friend.User[0]._id)) {
             users.push(friend.User[0]._id);
-            let myfriend={
-              fullName: `${friend.User[0].firstname} ${friend.User[0].lastname}`,
+            let myfriend = {
+              fullName: `${friend.User[0].firstname} ${
+                friend.User[0].lastname
+              }`,
               user_id: friend.User[0]._id,
-              pet: [friend]
-            }
+              pet: [friend],
+            };
             petFriends.push(myfriend);
           } else {
-            petFriends = petFriends.map((userpet) => {
-              if(userpet.user_id===friend.User[0]._id) {
+            petFriends = petFriends.map(userpet => {
+              if (userpet.user_id === friend.User[0]._id) {
                 userpet.pet.push(friend);
                 return userpet;
               } else {
@@ -40,41 +40,66 @@ class HomeRight extends React.Component {
           }
         });
         this.setState({
-          petFriends: petFriends
+          petFriends: petFriends,
         });
       }
-    }
-  }
-  }
+  };
 
-  render () {
+  render() {
     return (
-
       <div className="row">
-        <div className="col-sm-12" style={{width: '250px', marginLeft: '80px', position: 'fixed', top: '12%', bottom: '0', borderLeft: '1px solid lightgray'}}>
+        <div
+          className="col-sm-12"
+          style={{
+            width: "250px",
+            marginLeft: "80px",
+            position: "fixed",
+            top: "12%",
+            bottom: "0",
+            borderLeft: "1px solid lightgray",
+          }}
+        >
           <div className="pl-3">
-            <h4 style={{marginTop: '25%', paddingTop: '70px', paddingBottom: '20px'}}>Following:</h4>       
-            {this.state.petFriends.map((friend) => {
+            <h4
+              style={{
+                marginTop: "25%",
+                paddingTop: "70px",
+                paddingBottom: "20px",
+              }}
+            >
+              Following:
+            </h4>
+            {this.state.petFriends.map(friend => {
               return (
                 <div key={friend._id}>
-                  <h5 style={{paddingBottom: '20px'}}><strong>{`${friend.fullName}`}</strong></h5>
+                  <h5 style={{ paddingBottom: "20px" }}>
+                    <strong>{`${friend.fullName}`}</strong>
+                  </h5>
                   {friend.pet.map(pet => {
                     return (
-                    <Link className="text-dark" key={pet._id} to={`/profile/${pet._id}`} >
-                      <img src="http://via.placeholder.com/40x40" className="img-responsive rounded-circle petImage mr-3" alt="Pet" />
-                      <span>{pet.name}</span>
-                      <br /><br />
-                    </Link>
+                      <Link
+                        className="text-dark"
+                        key={pet._id}
+                        to={`/profile/${pet._id}`}
+                      >
+                        <img
+                          src="http://via.placeholder.com/40x40"
+                          className="img-responsive rounded-circle petImage mr-3"
+                          alt="Pet"
+                        />
+                        <span>{pet.name}</span>
+                        <br />
+                        <br />
+                      </Link>
                     );
                   })}
                 </div>
-              )
+              );
             })}
-          </div> 
+          </div>
         </div>
       </div>
     );
-
   }
 }
 
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
@@ -8,10 +7,9 @@ const dbConnection = require("./db");
 const passport = require("./passport");
 const routes = require("./routes");
 const dotenv = require("dotenv");
+dotenv.config();
 const path = require("path");
 const db = require("./db/models");
-
-dotenv.config();
 
 // Initialize Expressasdsad
 const app = express();
@@ -28,33 +26,26 @@ app.use(bodyParser.json());
 // import models from "./models";
 
 app.use(
-	session({
-		secret: process.env.APP_SECRET || 'Shh, Colin is a super-hero!',
-		// store: new MongoStore({ mongooseConnection: dbConnection }),
-		resave: false,
-		saveUninitialized: false
-	})
-)
+  session({
+    secret: process.env.APP_SECRET || "Shh, Colin is a super-hero!",
+    // store: new MongoStore({ mongooseConnection: dbConnection }),
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
 
 // ======Passport======
 app.use(passport.initialize());
 app.use(passport.session());
 
-//Express app Routing
-app.use(routes);
-
-// ====== Error handler ====
-app.use(function(err, req, res, next) {
-	console.log(`====== ERROR =======`);
-	console.error(err.stack);
-	res.status(500);
-})
-
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production ") {
   console.log(`YOU ARE IN THE PRODUCTION ENV`);
   app.use(express.static("./client/build"));
 }
+
+//Express app Routing
+app.use(routes);
 
 // Start the server
 app.listen(PORT, function() {
